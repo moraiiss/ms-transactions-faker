b8b345e038270474340f02c8e575bad6456f2f89
Junior Morais <demoraisjunior@outlook.com>
2024-03-30 12:19:39 -0300

    fix: create kafka topic

---
+++ README.md
@@ -0,0 +1,8 @@
+## todo
+
+- [ ] verificar tratamento de erros 
+- [ ] validação de regras de negócios
+- [ ] implementar lógica de autorização mockada
+- [ ] implementar lógica de notificação
+- [ ] swagger 
+- [ ] implementar logger da aplicação
--- src/main/kotlin/com/transactions/MsTransactionsFakerApplication.kt
+++ src/main/kotlin/com/transactions/MsTransactionsFakerApplication.kt
@@ -1,12 +1,21 @@
 package com.transactions
 
+import org.apache.kafka.clients.admin.NewTopic
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.data.jdbc.repository.config.EnableJdbcAuditing
+import org.springframework.context.annotation.Bean
+import org.springframework.kafka.config.TopicBuilder
 
 @SpringBootApplication
 class MsTransactionsFakerApplication
 
 fun main(args: Array<String>) {
     runApplication<MsTransactionsFakerApplication>(*args)
+}
+
+@Bean
+fun notificationTopic(): NewTopic {
+    return TopicBuilder
+        .name("transaction-notification")
+        .build();
 }
