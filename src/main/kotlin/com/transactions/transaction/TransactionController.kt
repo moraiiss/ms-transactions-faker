345645d370117234404ecd12ab445d4b96ab185d
Junior Morais <demoraisjunior@outlook.com>
2024-03-31 11:29:34 -0300

    feat: add swagger

--- src/main/kotlin/com/transactions/transaction/TransactionController.kt
+++ src/main/kotlin/com/transactions/transaction/TransactionController.kt
@@ -1,5 +1,7 @@
 package com.transactions.transaction
 
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.tags.Tag
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
@@ -7,16 +9,19 @@ import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-@RequestMapping("transactions")
+@RequestMapping("api/v1/transactions")
+@Tag(name = "transactions", description = "Endpoints for managing transactions")
 class TransactionController (
     val service: TransactionService
 ) {
     @PostMapping
+    @Operation(summary = "Create transaction", description = "Returns the created transaction")
     fun createTransaction(@RequestBody transaction: Transaction): Transaction {
         return service.create(transaction)
     }
 
     @GetMapping
+    @Operation(summary = "List all transactions", description = "Return a list of all transactions")
     fun listTransactions(): MutableList<Transaction> {
         return service.list()
     }
